5cbb9ddef2943d743d237d9ffa15f62ab387a107
DiegoQuirozMasferrer <diego.quiroz.12.14@gmail.com>
2021-05-31 16:39:18 -0400

    jam

---
+++ New Unity Project/Assets/FsmState.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FsmState : MonoBehaviour
+{
+    enum state
+    {
+        Wander, 
+        espera,
+        cambio,
+        ataque
+
+    }
+    state CurrentState;
+    IEnumerator FSM()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(CurrentState.ToString());
+
+        }
+    }
+
+    void ChangeState(state nextState)
+    {
+        Debug.Log(CurrentState + "->" + nextState);
+        CurrentState = nextState;
+    }
+    IEnumerator Wander ()
+    {
+        while (CurrentState == state.Wander)
+        {
+
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project/Assets/movientoplayer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movientoplayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+        // Start is called before the first frame update
+    private Rigidbody2D rb;
+    Vector2 input;
+    public float speed;
+    private float position;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = input * speed * Time.fixedDeltaTime;
+        position = gameObject.transform.position.y;
+        
+        if (Input.GetKey(KeyCode.UpArrow)  )
+        {
+            gameObject.transform.localScale = new Vector3(1.2f, 1.4f);
+            Trampa();
+            Debug.Log("se achico");
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.localScale = new Vector3(3.18f, 2.6625f);
+            Legal();
+        }
+
+    }
+    private bool Trampa()
+    {
+        gameObject.transform.position = new Vector2( gameObject.transform.position.x ,1.674391f);
+        return true;
+
+    }    
+    private void Legal()
+    {
+        gameObject.transform.position = new Vector2(gameObject.transform.position.x, -3.15f);
+    }
+}
+
+    // Update is called once per frame
+    
+
