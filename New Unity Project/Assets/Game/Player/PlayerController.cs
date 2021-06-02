ce0e92df23f8f381d2fa848e753ca0d256b867f2
TotteCloud <80776911+TotteCloud@users.noreply.github.com>
2021-06-02 18:12:57 -0400

    RuleBreakerv0.4
    + PlayerController

---
+++ New Unity Project/Assets/Game/Player/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJump;
+    public int extraJumpValue;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        if(isGrounded == true) 
+        {
+            extraJump = extraJumpValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJump > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJump--;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJump == 0 && isGrounded) 
+        {
+            rb.velocity = Vector2.up * jumpForce; 
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+         
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (!facingRight && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight && moveInput < 0) 
+        {
+            Flip();
+        }
+    }
+
+    void Flip() 
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+}
